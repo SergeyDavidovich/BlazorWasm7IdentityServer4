8ff4793d7208291754463faff9bc7c91223ba8fb
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-03-31 12:38:36 +0200

    Add project files.

---
+++ BlazorWasm7IdentityServer4/Client/Program.cs
@@ -0,0 +1,27 @@
+using BlazorWasm7IdentityServer4.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorWasm7IdentityServer4.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddHttpClient("BlazorWasm7IdentityServer4.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWasm7IdentityServer4.ServerAPI"));
+
+            builder.Services.AddApiAuthorization();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ BlazorWasm7IdentityServer4/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWasm7IdentityServer4.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
